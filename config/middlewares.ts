f78877a1f058b92c26ef9c1797da21071ca49653
mk <kamai122000@mail.ru>
2026-02-18 17:06:25 +0300

    strapi::cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,16 +1,27 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from '@strapi/strapi'
 
 const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+	'strapi::logger',
+	'strapi::errors',
+	'strapi::security',
+	'strapi::poweredBy',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
+	{
+		name: 'strapi::cors',
+		config: {
+			enabled: true,
+			headers: '*',
+			origin: [
+				'http://localhost:5173',           // локальный Vite
+				'http://localhost:3000',
+				'https://home-build-ten.vercel.app', // ваш хост на Vercel
+			],
+		},
+	},
+]
 
-export default config;
+export default config
